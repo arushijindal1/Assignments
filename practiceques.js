502c515fd6d4c0258ead98ccc46407a7d3221d8b
arushijindal <aarujindal1@gmail.com>
2018-06-10 21:23:48 +0530

    filesadded

---
+++ practiceques.js
@@ -0,0 +1,56 @@
+var a = 1
+var b = 2
+console.log(a)
+console.log(b)
+
+function mul (a,b)
+{
+    return a*b
+}
+console.log(mul(2,3))
+
+var c = 'arushi'
+var d = 'jindal'
+var e = c+d
+console.log(e)
+console.log(e.length)
+
+var str = "please locate where locate occurs!";
+console.log(str.indexOf('w')) 
+
+var str="Hello World"
+console.log(str.toUpperCase())
+
+var str ="hello world"
+console.log(str.toLowerCase())
+
+var text="hello"
+var tex = text.split("m ")
+console.log(tex)
+
+var fruits=["banana","orange","apple","mango"]
+fruits.push("kiwi")
+console.log(fruits)
+fruits.pop("apple")
+console.log(fruits)
+fruits.splice(2,1)
+console.log(fruits)
+
+console.log(fruits.indexOf("orange"))
+
+console.log(typeof fruits)
+
+fruits[3]="abc"
+console.log(fruits)
+
+var veggies=["a","b","c"]
+console.log(veggies.toString(veggies))
+
+var x=9.656
+console.log(x.toString())
+
+var x = 7.656
+console.log(x.toPrecision(2))
+
+var x=53.32
+console.log("t")
